e786e257906078315d9cf1517d9472c9be012379
Osaze112 <ikponmwenosazee112@gmail.com>
2022-12-08 02:17:20 +0000

    learning

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for j in range(26):
-   if (j!= 4 and j!= 16):
-       print("{}".format((chr(j)), sep="", end="")
+     if (j! = 4 and j! = 16):
+          print("{}".format((chr(j)), sep="", end="")
