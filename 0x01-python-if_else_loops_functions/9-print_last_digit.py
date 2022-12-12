eefc4258c75bb799942fcfb87a2032d32e100455
Osaze112 <ikponmwenosazee112@gmail.com>
2022-12-12 13:39:32 +0000

    learning

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-         pe = number%10
+         pi = number%10
     else:
        pi = number% -10
        pi = pi *-1
+    print("{:d}".format(ld), end='')
     return pi
   
