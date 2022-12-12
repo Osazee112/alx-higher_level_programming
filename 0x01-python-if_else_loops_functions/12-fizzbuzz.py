f4c388be01205a4462954344ba20c5e350207a4d
Osaze112 <ikponmwenosazee112@gmail.com>
2022-12-12 14:36:12 +0000

    learning

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,10 @@
 #!/bin/usr/python3
-for i in range(1,101)
-   if i % 3 == 0:
-       print("Fizz")
-   elif i % 5 == 0:
-       print("Buzz")
-   else:
-       print("FizzBuzz")
+def fizzbuzz():
+    for i in range(1,101)
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print("FizzBuzz")
    
