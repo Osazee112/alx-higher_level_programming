f5fa125ada4ad71eb3983aff14e64f60b4f77b25
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-12-18 16:51:01 -0800

    keep

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv) - 1
     if num == 0:
-        print("0 argument.s")
+        print("0 arguments.")
     elif num == 1:
         print("1 argument:")
     else:
