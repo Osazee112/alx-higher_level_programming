5ec9f1f0820b3169135f36b56dec113e01f50d53
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-12-18 17:23:13 -0800

    keep

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     result = 0
     count = len(sys.argv) - 1
     for j in range(count):
-        result += int(sys.argv[j + 1]
-        print(result)
+        result += int(sys.argv[j + 1])
+    print(result)
