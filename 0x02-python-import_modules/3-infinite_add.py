171a76f8f98f761efe1ca6451b30e4293d530061
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-12-18 17:16:26 -0800

    keep

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    result = 0
+    count = len(sys.argv) - 1
+    for j in range(count):
+        result += int(sys.argv[j + 1]
+        print(result)
