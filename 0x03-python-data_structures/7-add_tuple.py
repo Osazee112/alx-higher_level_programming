6ef7879d090bab56ad77e2faa7af5e2af35c44de
Osazee112 <ikponmwenosazeepeter112@mail.com>
2023-01-01 19:49:26 -0800

    addtuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(),tuple_b=()):
-     tuple1 = list(tuple_a)
-     tuple2 = list(tuple_b)
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple1 = list(tuple_a)
+    tuple2 = list(tuple_b)
      re_tuple=[]
      for i in range(len(tuple_a)):
         if i <=1:
